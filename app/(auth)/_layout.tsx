ed06c0437e55591dae35784363ea948641f4793a
Assane <105631882+asndiallo@users.noreply.github.com>
2024-08-20 19:13:42 -0400

    feat: Add authentication screens and navigation

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+
+const layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="register" options={{ headerShown: false }} />
+    </Stack>
+  );
+};
+
+export default layout;
