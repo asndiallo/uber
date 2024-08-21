16d3a549939b3418ba85217b568a0f1c6b419825
Assane <105631882+asndiallo@users.noreply.github.com>
2024-08-21 17:12:16 -0400

    feat: Add RideCard component for displaying ride details
    This commit adds a new component called RideCard, which is responsible for displaying detailed information about a ride. The RideCard component takes in a Ride object as a prop and renders the destination address, origin address, date and time, driver information, and payment status. This component enhances the user experience by providing a visually appealing and informative display of ride details.

---
+++ libs/utils.ts
@@ -0,0 +1,61 @@
+import { Ride } from "@/types/type";
+
+/**
+ * Sort rides by date
+ * @param rides - Array of rides
+ * @returns Sorted array of rides
+ */
+export const sortRides = (rides: Ride[]): Ride[] => {
+  const result = rides.sort((a, b) => {
+    const dateA = new Date(`${a.created_at}T${a.ride_time}`);
+    const dateB = new Date(`${b.created_at}T${b.ride_time}`);
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  return result.reverse();
+};
+
+/**
+ * Format time to human readable format
+ * @param minutes - Time in minutes
+ * @returns Formatted time
+ */
+export function formatTime(minutes: number): string {
+  const formattedMinutes = +minutes?.toFixed(0) || 0;
+
+  if (formattedMinutes < 60) {
+    return `${minutes} min`;
+  } else {
+    const hours = Math.floor(formattedMinutes / 60);
+    const remainingMinutes = formattedMinutes % 60;
+    return `${hours}h ${remainingMinutes}m`;
+  }
+}
+
+/**
+ * Format date to human readable format
+ * @param dateString - Date string
+ * @returns Formatted date
+ */
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day < 10 ? "0" + day : day} ${month} ${year}`;
+}
