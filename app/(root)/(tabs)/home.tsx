5ce1d3981c50e2f3847cfa4b38454db98a14702a
Assane <105631882+asndiallo@users.noreply.github.com>
2024-08-21 17:46:11 -0400

    Add empty component handling as well as loading resources

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,9 +1,10 @@
-import { FlatList, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
 import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
 
 import { Link } from "expo-router";
 import RideCard from "@/components/RideCard";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 const recentRides = [
   {
@@ -114,12 +115,33 @@ const recentRides = [
 
 export default function Page() {
   const { user } = useUser();
+  const loading = false;
 
   return (
     <SafeAreaView className="bg-general-500">
       <FlatList
         data={recentRides?.slice(0, 5)}
         renderItem={({ item }) => <RideCard ride={item} />}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ paddingBottom: 100 }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image
+                  source={images.noResult}
+                  className="w-40 h-40"
+                  alt="No recent rides found"
+                  resizeMode="contain"
+                />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
       />
     </SafeAreaView>
   );
