17c213a34fa6a86b580a8e04feebc84c1133cd76
Assane <105631882+asndiallo@users.noreply.github.com>
2024-08-20 20:38:10 -0400

    chore: Add lazy loading for images and update welcome screen

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -4,6 +4,7 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constants";
 
 const Home = () => {
@@ -52,6 +53,16 @@ const Home = () => {
           </View>
         ))}
       </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/register")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10 mb-5"
+      />
     </SafeAreaView>
   );
 };
