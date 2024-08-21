d44a90a1ea85fb96b41bd7ca7fb5513421406ae0
Assane <105631882+asndiallo@users.noreply.github.com>
2024-08-21 19:17:05 -0400

    feat: Add Map component for displaying a map with user and destination locations

---
+++ components/Map.tsx
@@ -0,0 +1,38 @@
+import MapView, { PROVIDER_DEFAULT } from "react-native-maps";
+import { Text, View } from "react-native";
+
+import { calculateRegion } from "@/libs/map";
+import useLocationStore from "@/store";
+
+const Map = () => {
+  const {
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  } = useLocationStore();
+
+  const region = calculateRegion({
+    userLatitude,
+    userLongitude,
+    destinationLatitude,
+    destinationLongitude,
+  });
+
+  return (
+    <MapView
+      provider={PROVIDER_DEFAULT}
+      className="w-full h-full rounded-2xl"
+      tintColor="black"
+      mapType="mutedStandard"
+      showsPointsOfInterest={false}
+      initialRegion={region}
+      showsUserLocation={true}
+      userInterfaceStyle="light"
+    >
+      <Text>Map</Text>
+    </MapView>
+  );
+};
+
+export default Map;
---
+++ store/index.ts
@@ -0,0 +1,47 @@
+import { LocationStore } from "@/types/type";
+import { create } from "zustand";
+
+/**
+ * Store for user and destination location data
+ * @returns userAddress, userLongitude, userLatitude, destinationLongitude, destinationLatitude, destinationAddress, setUserLocation, setDestinationLocation
+ */
+const useLocationStore = create<LocationStore>((set) => ({
+  userAddress: null,
+  userLongitude: null,
+  userLatitude: null,
+  destinationLongitude: null,
+  destinationLatitude: null,
+  destinationAddress: null,
+  setUserLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      userLatitude: latitude,
+      userLongitude: longitude,
+      userAddress: address,
+    }));
+  },
+  setDestinationLocation: ({
+    latitude,
+    longitude,
+    address,
+  }: {
+    latitude: number;
+    longitude: number;
+    address: string;
+  }) => {
+    set(() => ({
+      destinationLatitude: latitude,
+      destinationLongitude: longitude,
+      destinationAddress: address,
+    }));
+  },
+}));
+
+export default useLocationStore;
